2b343b9e239b7dcbec1da5a7df744ac479f27a81
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2020-05-09 10:58:23 -0400

    Adicionado configuração do CORS ao backend

--- backend/src/main/java/br/com/desafio/api/controller/LicitacaoController.java
+++ backend/src/main/java/br/com/desafio/api/controller/LicitacaoController.java
@@ -9,6 +9,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import br.com.desafio.domain.repository.LicitacaoRepository;
 import br.com.desafio.domain.service.LicitacaoService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/licitacao")
 public class LicitacaoController {
 
